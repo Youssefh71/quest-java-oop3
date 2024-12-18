068fc82d5226ee8b5945ef7f432fe1c8a655a9e6
Youssefh71 <youssefhourri69@gmail.com>
2024-12-18 18:01:28 +0100

    interface

--- Eagle.java
+++ Eagle.java
@@ -1,6 +1,6 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Flyable {
 
-    private boolean flying;
+    private final boolean flying;
     private int altitude;
 
     public Eagle(String name) {
@@ -20,5 +20,42 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(getName() + " takes off in the sky.");
+
+    }
+
+    @Override
+    public void ascend(int meters) {
+        altitude += meters;
+        System.out.println(getName() + " flies upward, altitude: " + altitude);
+
+    }
+
+    @Override
+    public void descend(int meters) {
+        altitude -= meters;
+        if (altitude < 0) altitude = 0;
+        System.out.println(getName() + " flies downward, altitude: " + altitude);
+
+    }
+
+    @Override
+    public void land() {
+        if (altitude > 1) {
+            System.out.println(getName() + " is too high, it can't land.");
+        } else {
+            System.out.println(getName() + " lands on the ground.");
+        }
+
+    }
+
+    @Override
+    public void glide() {
+        System.out.println("glides into the air.");
+
     }
 }
---
+++ Flyable.java
@@ -0,0 +1,7 @@
+public interface Flyable {
+    void takeOff(); // Décolle
+    void ascend(int meters); // Monte en altitude
+    void descend(int meters); // Descend en altitude
+    void land(); // Atterrit
+    void glide(); // Plane
+}
